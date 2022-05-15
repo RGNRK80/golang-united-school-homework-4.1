ce7f1407826d5f3b5361927ef930c656dc2f38a5
Ivan <92035014+RGNRK80@users.noreply.github.com>
2022-05-15 14:33:30 +0300

    2step

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,12 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	output = ReverseString(input)
+
+	runes := []rune(input)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	output = string(runes)
+
 	return output
 }
